9d5eaeb896d95f68d99b53a62c74651d1ecb6dce
megalai1234 <43366156+megalai1234@users.noreply.github.com>
2018-12-20 14:35:17 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+ {
+ int a;
+ printf("enter  the a value");
+ if(a>0)
+  {
+  printf(" positive number");
+  }
+ else
+  {
+    printf("odd number");
+   }
+getch();
+ }  
